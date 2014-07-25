c3e7779de6a236ffec5cb52b5c857e603dc8ce97
Dave Collier <lardelbow@gmail.com>
2014-07-25 23:22:30 +0100

    Video example completed

--- app/controllers/servlet_controller.rb
+++ app/controllers/servlet_controller.rb
@@ -1,6 +1,6 @@
 class ServletController < ApplicationController
   def echo
     @msg=params[:msg]
-    render 'servlet/echo.txt.erb', content_type: 'text/plain'
+    render 'servlet/echo.txt.erb', content_type: 'text/plain', status: 200
   end
 end
